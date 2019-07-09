4af9d003a42b15dadd0190dc09beb705eb572a2d
Andrew Gorovoy <andrewgroovy@gmail.com>
2019-07-09 14:30:25 -0600

    Completed core functionality of the tool. Need to add filters to the tables and general styling improvements.

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,9 +1,14 @@
 var coll = document.getElementsByClassName("collapsible");
 var i;
 var prodData = [];
+var filters = $("table.filter-section tbody");
+var watchLater = [];
+
+
+
+getProdMapData(populateFilters);
 
 $(document).ready(function() {
-    getProdMapData();
     filterTableListener();
     $('#filter input').on('click',filterTable);
 
@@ -21,7 +26,31 @@ $(document).ready(function() {
 
 
     $('#prodList td a.link-prod-info').on("click",showProdInfo);
+    $('#watchLater').on("click",addToWatchLater);
+    $('#watchNow').on('click',watchNow);
+
+    $("#defaultOpen").click();
 });
+
+function populateFilters() {
+  //for item in data
+  $.each(filters,function(){
+    var filterType = $(this).attr('rel');
+    var filterDict = {};
+    console.log(filterType);
+    console.log(prodData[i]);
+    for (var i = 0; i < prodData.length;i++)
+    {
+      console.log(prodData[i]['title']);
+    }
+  });
+    // check if in object
+      //if yes, then next item
+      //else add item to filter table
+
+
+
+}
 
 function activateSearch() {
   var input = $('#search');
@@ -40,17 +69,18 @@ function filterTable() {
   }
 }
 
-function getProdMapData() {
+function getProdMapData(_callback) {
   $.ajax(
     {
       url: 'users/100', 
       success: function(result){
         var prodList = result;
         prodData = prodList;
-        console.log(prodData);
+        console.log('hello');
       }
     }
   );
+  _callback();
 }
 
 // function displayNum(num) {
@@ -80,13 +110,15 @@ function displayNum(num) {
   for (var i = 0; i < num; i++)
   {
     tableContent += '<tr>';
-    tableContent += '<td><a href="#"  class="linkresinfo" rel="' + prodData[i]['id'] +'">' + prodData[i]['title'] + '</a></td>';
+    tableContent += '<td><a href="#"  class="link-prod-info" rel="' + prodData[i]['id'] +'">' + prodData[i]['title'] + '</a></td>';
     tableContent += '<td>' + prodData[i]['producer'] + '</td>';
     tableContent += '<td>' + prodData[i]['genre'] + '</td>';
     tableContent += '<td>' + prodData[i]['rating'] + '</td>';
     tableContent += '<tr>';
   }
   $('#prodList table tbody').html(tableContent);
+  $('#prodList td a.link-prod-info').on("click",showProdInfo);
+
 
 }
 
@@ -104,14 +136,19 @@ function filterTableListener() {
       }
 }
 
-function showProdInfo(e) {
-  e.preventDefault();
-
-  var prodID = $(this).attr('rel');
-
+function dataPosMap(prodID) {
   var pos = prodData.map(function(prod) {
     return prod.id;
   }).indexOf(prodID);
+
+  return pos;
+}
+
+function showProdInfo(e) {
+  openNav();
+  e.preventDefault();
+  var prodID = $(this).attr('rel');
+  var pos = dataPosMap(prodID);
 
   $('#prodInfoTitle').text(prodData[pos]['title']);
   $('#prodInfoYear').text(prodData[pos]['theaterdate']);
@@ -119,4 +156,61 @@ function showProdInfo(e) {
   $('#prodInfoGenre').text(prodData[pos]['genre']);
   $('#prodInfoRating').text(prodData[pos]['rating']);
   $('#prodInfoDesc').text(prodData[pos]['desc']);
+  $('.stream-actions').attr('rel',prodData[pos]['id'])
 }
+
+function openNav() {
+  $("#prodInfo").css('width','325px');
+  $('#main').css('marginRight','325px');
+}
+
+function closeNav() {
+  $("#prodInfo").css('width','0');
+  $('#main').css('marginRight','0');
+}
+
+function addToWatchLater(e) {
+  e.preventDefault();
+  var prodID = $(this).attr('rel');
+  var pos = dataPosMap(prodID);
+  watchLater.push(prodData[pos]);
+}
+
+function watchNow() {
+  alert("watching now");
+}
+
+function openTab(tableName, elmnt) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+
+  document.getElementById(tableName).style.display = "block";
+  if (tableName == "watchLaterTable") {
+    repopulateTable();
+  }
+
+  elmnt.style.backgroundColor = "powderblue";
+}
+
+function repopulateTable() {
+  var tableContent = '';
+  for (var i = 0; i < watchLater.length;i++)
+  {
+    tableContent += '<tr>';
+    tableContent += '<td><a href="#"  class="link-prod-info" rel="' + watchLater[i]['id'] +'">' + prodData[i]['title'] + '</a></td>';
+    tableContent += '<td>' + watchLater[i]['producer'] + '</td>';
+    tableContent += '<td>' + watchLater[i]['genre'] + '</td>';
+    tableContent += '<td>' + watchLater[i]['rating'] + '</td>';
+    tableContent += '<tr>';
+  }
+
+  $('#watchLaterTable table tbody').html(tableContent);
+};
