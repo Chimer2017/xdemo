425b4dd6f9d5b88c0a434764e9017a16bf084169
Andrew Gorovoy <andrewgroovy@gmail.com>
2019-07-15 16:11:58 -0600

    Chnaged filtering strategy. Now calling MVIS every time for filters.Only completed rating filter

--- assets/js/global.js
+++ assets/js/global.js
@@ -42,18 +42,50 @@ function searchTable(value) {
 
 function filterTable() {
   console.log("Recieved request to filter table!");
-  var value = $(this).attr('value');
-  var filter = $(this).attr('rel');
-  if ( filter == 'display-num') {
-    displayNum(value);
-    console.log(value);
-    $('span.filter-header.display-num').text("Displaying: " + value);
-  } else if ( filter == 'rating') {
-    value = value + "";
-    rateByFilter(value);
-  } else if (filter == 'genre') {
-    rateByGenre(value);
-  }
+  var filters = $("span.filter-header");
+  filters.splice(0,1);
+  var qryStr = "";
+  var value;
+  var triggerValue = $(this).attr('value');
+  var triggerFilter = $(this).attr('rel');
+  $.each(filters,function(){
+    console.log($(this).attr('name')+ ":"+$(this).attr('value'));
+    var filter = $(this).attr('name');
+    filter = filter.toLowerCase();
+    filter = filter.replace(/:/g,'');
+    value = $(this).attr('value');
+    if((value != "null") || (filter == triggerFilter)) {
+      
+      if (filter == 'rating') {
+        if ( value == 'null') value = triggerValue;
+        value = value.replace(/-/g,'');
+        if (value =="Not Rated") value = "NR";
+        var temp = filter + ' = "' + value + '"';
+        qryStr += temp;
+
+      }
+
+      if 
+
+    }
+    
+  });
+  
+  console.log(qryStr);
+  $.getJSON('/users/filter/' + qryStr,function(data){
+    repopulateTable(data,'all');
+  });
+
+
+  // if ( filter == 'display-num') {
+  //   displayNum(value);
+  //   $('span.filter-header.display-num').text("Displaying: " + value);
+  // } else if ( filter == 'rating') {
+  //   value = value + "";
+  //   rateByFilter(value);
+  // } else if (filter == 'genre') {
+  //   rateByGenre(value);
+  // }
 }
 
 function getProdMapData() {
@@ -86,7 +118,6 @@ function rateByFilter(value) {
     var rating = $(this).text();
     rating = rating.toLowerCase();
     rating = rating.replace(/-/g,'');
-    console.log(rating);
     if (rating === searchStr) {
       $(this).parent().show();
     } else {
@@ -104,7 +135,6 @@ function rateByGenre(value) {
     var tds = $("#prodList tr td.genre");
     $.each(tds,function() {
         var genre = $(this).text();
-        console.log(genre+ "   :   " + searchStr);
         if (genre === searchStr) {
         $(this).parent().show();
         } else {
@@ -123,31 +153,52 @@ function dataPosMap(prodID) {
   return pos;
 }
 
+// function showProdInfo(e) {
+//   e.preventDefault();
+//   var prodID = $(this).attr('rel');
+//   var pos = dataPosMap(prodID);
+
+//   if (!checkButtonDisabled(prodID)) {
+//       $('#watchLater').removeClass('disabled');
+//   } else {
+//     $('#watchLater').addClass('disabled');
+//   }
+
+//   $('#prodInfoTitle').text(prodData[pos]['title']);
+//   $('#prodInfoYear').text(prodData[pos]['theaterdate']);
+//   $('#prodInfoProducer').text(prodData[pos]['producer']);
+//   $('#prodInfoGenre').text(prodData[pos]['genre']);
+//   $('#prodInfoRating').text(prodData[pos]['rating']);
+//   $('#prodInfoDesc').text(prodData[pos]['desc']);
+//   $('.stream-actions').attr('rel',prodData[pos]['id'])
+// }
+
 function showProdInfo(e) {
-  e.preventDefault();
-  var prodID = $(this).attr('rel');
-  var pos = dataPosMap(prodID);
-
-  if (!checkButtonDisabled(prodID)) {
-      $('#watchLater').removeClass('disabled');
-  } else {
-    $('#watchLater').addClass('disabled');
-  }
-
-  $('#prodInfoTitle').text(prodData[pos]['title']);
-  $('#prodInfoYear').text(prodData[pos]['theaterdate']);
-  $('#prodInfoProducer').text(prodData[pos]['producer']);
-  $('#prodInfoGenre').text(prodData[pos]['genre']);
-  $('#prodInfoRating').text(prodData[pos]['rating']);
-  $('#prodInfoDesc').text(prodData[pos]['desc']);
-  $('.stream-actions').attr('rel',prodData[pos]['id'])
+    var prodData;
+    e.preventDefault();
+    var prodID = $(this).attr('rel');
+    $.getJSON('/users/prodID/' + prodID,function(data) {
+        $('#prodInfoTitle').text(data['title']);
+        $('#prodInfoYear').text(data['theaterdate']);
+        $('#prodInfoProducer').text(data['producer']);
+        $('#prodInfoGenre').text(data['genre']);
+        $('#prodInfoRating').text(data['rating']);
+        $('#prodInfoDesc').text(data['desc']);
+        $('.stream-actions').attr('rel',data['id']);
+    });
+  
+    if (!checkButtonDisabled(prodID)) {
+        $('#watchLater').removeClass('disabled');
+    } else {
+      $('#watchLater').addClass('disabled');
+    }
+  
 }
 
 function checkButtonDisabled(prodID) {
     for (var i = 0; i < watchLater.length;i++)
     {
         if (watchLater[i]['id'] == prodID) {
-            console.log("gedmdke");
             return true;
         }
     }
@@ -160,10 +211,18 @@ function checkButtonDisabled(prodID) {
 function addToWatchLater(e) {
   e.preventDefault();
   var prodID = $('span.stream-actions').attr('rel');
-  var pos = dataPosMap(prodID);
-  watchLater.push(prodData[pos]);
-  $(this).addClass('disabled');
-  repopulateTable(watchLater,'later');
+  // if (!checkButtonDisabled(prodID)) {
+  //   $('#watchLater').removeClass('disabled');
+  // } else {
+  //   $('#watchLater').addClass('disabled');
+  // }
+  if (!checkButtonDisabled(prodID)) {
+    var pos = dataPosMap(prodID);
+    watchLater.push(prodData[pos]);
+    $(this).addClass('disabled');
+    repopulateTable(watchLater,'later');
+  }
+  
 }
 
 function watchNow() {
--- routes/users.js
+++ routes/users.js
@@ -25,6 +25,38 @@ router.get('/:num', function(req, response, next) {
   
 });
 
+router.get('/prodID/:prodID',function(req,response){
+  var prodList = [];
+  var id = req.params.prodID;
+  console.log(id);
+  var URL = 'http://den-vm-eng142.rocketsoftware.com:7171/Xdemo/Products/' + id ;
+  request(URL, { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    response.send(body);
+  }).auth('agorovoy@rs.com','admin',true);
+});
+
+
+router.get('/filter/:query',function(req,response){
+  var searchQuery = req.params.query;
+  var prodList = [];
+  // var URL = 'http://den-vm-eng142.rocketsoftware.com:7171/Xdemo/Products?select=rating%20%3D%20%22R%22';
+  var URL = 'http://den-vm-eng142.rocketsoftware.com:7171/Xdemo/Products?select=' + searchQuery;
+  console.log(URL);
+  request(URL, { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.Products.forEach(el => { 
+      prodList.push(el);
+    });
+    prodData = prodList;
+    response.send(prodList);
+  
+    //response.send(body);
+  }).auth('agorovoy@rs.com','admin',true);
+
+
+
+});
 
 
 
